c058d630bbe0af74260f7f6fd405b71d72dd6ab5
arun06001 <arun06001@E-PW0234KE>
2023-11-22 12:35:26 +0100

    Add project files.

---
+++ Uppgift_5.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Uppgift_5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] talArray = new int[3];
+
+            Console.WriteLine("Skriv in tre heltal på var sin rad");
+
+            for (int i = 0; i < talArray.Length; i++)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        talArray[i] = int.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Det är inte ett giltigt svar. Försök igen");
+                    }
+                }
+            }
+            
+            int summa = 0;
+
+            foreach (int tal in talArray)
+            {
+                summa += tal;
+            }
+
+            Console.WriteLine($"\nSumman av de talen är {summa}.\nTryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
